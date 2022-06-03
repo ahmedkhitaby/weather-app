01ea236669d4dcfdf64257efb289fd6eb00bbb91
Ahmed Khitaby <ahmedkhitaby@gmail.com>
2022-06-04 00:09:40 +0200

    change apis urls from http to https

--- src/apis/weather.ts
+++ src/apis/weather.ts
@@ -5,9 +5,9 @@ export const getWeatherByCoordinatesApiUrl = (
   if (latitude === 0 || longitude === 0) {
     return "";
   }
-  return `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${latitude},${longitude}&includelocation=yes&showlocaltime=yes&format=json&num_of_days=8`;
+  return `https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${latitude},${longitude}&includelocation=yes&showlocaltime=yes&format=json&num_of_days=8`;
 };
 
 export const getWeatherByCityApiUrl = (city: string) => {
-  return `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}&includelocation=yes&showlocaltime=yes&format=json&num_of_days=8`;
+  return `https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}&includelocation=yes&showlocaltime=yes&format=json&num_of_days=8`;
 };
